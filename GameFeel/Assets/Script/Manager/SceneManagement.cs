631e5706373f09eaee4540be752600a7d300fe32
Julien <julien.aberton@edu.devinci.fr>
2023-11-16 09:54:07 +0100

    UI

--- GameFeel/Assets/Script/Manager/SceneManagement.cs
+++ GameFeel/Assets/Script/Manager/SceneManagement.cs
@@ -6,6 +6,14 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagement : MonoBehaviour
 {
+    [SerializeField] GameStatsRef _ref;
+    ISet<int> RealRef => _ref;
+
+    private void Awake()
+    {
+        RealRef.Set(0);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
