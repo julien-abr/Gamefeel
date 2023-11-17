b5dbf9b14cc68f6bc227c83521ebaee093ffef8d
Artyu24 <65313164+Artyu24@users.noreply.github.com>
2023-11-17 09:59:26 +0100

    [UP] Movement on Flower

--- GameFeel/Assets/Script/Invader/FlowerSpawnManager.cs
+++ GameFeel/Assets/Script/Invader/FlowerSpawnManager.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using NaughtyAttributes;
 using System.Collections;
 using System.Collections.Generic;
@@ -9,6 +10,8 @@ public class FlowerSpawnManager : MonoBehaviour
     [SerializeField, ReadOnly] private List<SpriteRenderer> _flowerRendererList = new List<SpriteRenderer>();
 
     [SerializeField] private List<Sprite> _flowerSprites = new List<Sprite>();
+
+    [SerializeField] private float animTime = 5f;
 
     [Button]
     public void CreateNewFlowerSlot()
@@ -48,6 +51,20 @@ public class FlowerSpawnManager : MonoBehaviour
             int s = Random.Range(0, _flowerSprites.Count);
             _flowerRendererList[r].sprite = _flowerSprites[s];
         }
+
+        Sequence seq = DOTween.Sequence();
+
+        Tween a = _flowerRendererList[r].transform.DOLocalRotate(new Vector3(0, 0, 120), animTime).SetEase(Ease.Linear);
+        Tween b = _flowerRendererList[r].transform.DOLocalRotate(new Vector3(0, 0, 240), animTime).SetEase(Ease.Linear);
+        Tween c = _flowerRendererList[r].transform.DOLocalRotate(new Vector3(0, 0, 360), animTime).SetEase(Ease.Linear);
+        //Tween d = _flowerRendererList[r].transform.DORotate(new Vector3(0, 0, 0), 0);
+        //_flowerRendererList[r].transform.do
+        seq.Append(a).Append(b).Append(c).SetLoops(-1);
+
+        Vector3 scale = _flowerRendererList[r].transform.localScale;
+        _flowerRendererList[r].transform.localScale = Vector3.zero;
+        _flowerRendererList[r].transform.DOScale(scale, 0.5f);
+        
         _flowerRendererList[r].enabled = true;
         _flowerRendererList.RemoveAt(r);
     }
